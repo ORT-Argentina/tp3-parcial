04be4460c22e4dd196127dc5b307ad60e81af290
dtlucho <luciano.ditomaso@gmail.com>
2022-05-31 23:23:23 -0300

    drawer

--- app/src/main/java/com/example/parcialgrupo2/activities/ContainerActivity.kt
+++ app/src/main/java/com/example/parcialgrupo2/activities/ContainerActivity.kt
@@ -1,28 +1,75 @@
 package com.example.parcialgrupo2.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.example.parcialgrupo2.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class ContainerActivity : AppCompatActivity() {
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
 
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.bottom_nav_host_fragment) as NavHostFragment
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.bottom_nav_host_fragment) as NavHostFragment
         bottomNavView = findViewById(R.id.bottom_nav_view)
 
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
+        navView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, _, _ ->
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_launcher_foreground)
+        }
+
+        drawerLayout = findViewById(R.id.drawer_layout_id)
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            navHostFragment.navController,
+            appBarConfiguration
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        Log.d("Test", prefs.getBoolean("switch_preference", true).toString())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return false
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/parcialgrupo2/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/parcialgrupo2/fragments/HomeFragment.kt
@@ -1,18 +1,26 @@
 package com.example.parcialgrupo2.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.parcialgrupo2.R
 class HomeFragment : Fragment() {
+    private lateinit var v: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        setHasOptionsMenu(true)
+
+        return v
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
