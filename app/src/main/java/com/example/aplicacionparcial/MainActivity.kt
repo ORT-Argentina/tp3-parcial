c7ec296a328bc51c6c1da2627dda8b2dd0f033c9
ovidiorava <osaccani@rava.com>
2022-06-28 20:15:12 -0300

    first navigation

--- app/src/main/java/com/example/aplicacionparcial/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionparcial/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.aplicacionparcial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSplash : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnSplash = findViewById(R.id.btnSplash)
+        btnSplash.setOnClickListener{
+            var intentNav = Intent(this, SecondActivity::class.java)
+            startActivity(intentNav)
+            this.finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/aplicacionparcial/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.aplicacionparcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuInflater
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+    //override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    //    var inflate: MenuInflater = menuInflater
+   //     inflate.inflate(R.menu.menu_home, menu)
+    //    return true
+   // }
+
+
+}
