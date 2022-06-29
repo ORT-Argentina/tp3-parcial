df8f094aad4f96cbe2ce829288e64ae7cf7fef47
ovidiorava <osaccani@rava.com>
2022-06-28 21:19:33 -0300

    navigation bottom done

--- app/src/main/java/com/example/aplicacionparcial/SecondActivity.kt
+++ app/src/main/java/com/example/aplicacionparcial/SecondActivity.kt
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SecondActivity : AppCompatActivity() {
 
+    lateinit var fcvHome: NavHostFragment
+    lateinit var bnvHome: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
+        fcvHome = supportFragmentManager.findFragmentById(R.id.fcvHome) as NavHostFragment
+        bnvHome=findViewById(R.id.bnvHome)
+        NavigationUI.setupWithNavController(bnvHome,fcvHome.navController )
     }
 
 
