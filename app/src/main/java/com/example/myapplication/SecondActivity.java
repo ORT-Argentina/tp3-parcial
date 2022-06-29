742a4c1e4950496048f12b1d493554cc374847c0
Mac-Kett <maca91@hotmail.com>
2022-06-28 22:59:41 -0300

    changes

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,14 +1,71 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class SecondActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        bottomNavBar = findViewById(R.id.bottomNavigationView);
+
+
+        bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.homeFragment:
+                        ft.replace(R.id.fragmentContainerView, new homeFragment()).commit();
+                        break;
+
+                    case R.id.otherScreenFragment:
+                        ft.replace(R.id.fragmentContainerView, new otherScreenFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        FragmentManager fm = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) fm.findFragmentById(R.id.nav_host);
+        bottomNavBar = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavBar, navHostFragment.getNavController());
+
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        selectedItemNav(item);
+        return true;
+    }
+
+    private void selectedItemNav(MenuItem item) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.homeFragment:
+                ft.replace(R.id.fragmentContainerView, new homeFragment()).commit();
+                break;
+
+            case R.id.otherScreenFragment:
+                ft.replace(R.id.fragmentContainerView, new otherScreenFragment()).commit();
+                break;
+        }
     }
 }
