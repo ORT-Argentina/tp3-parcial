d3fd6a26e9249c85517994bf5fcf574809121ad9
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-06-29 20:15:27 -0300

    funciona navegacion

--- app/src/main/java/com/example/myapplication/second.kt
+++ app/src/main/java/com/example/myapplication/second.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class second : AppCompatActivity() {
+
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var navHostFragment : NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+
+        bottomNavView = findViewById(R.id.bottomNav)
+
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu1, menu)
+        return true
+    }
+
+
 }
