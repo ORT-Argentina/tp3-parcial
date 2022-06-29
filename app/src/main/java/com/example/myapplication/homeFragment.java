42edda4229c7a8117856ade150ec30ff204705e9
Mac-Kett <maca91@hotmail.com>
2022-06-28 21:41:06 -0300

    Splash y otros archivos

--- app/src/main/java/com/example/myapplication/homeFragment.java
+++ app/src/main/java/com/example/myapplication/homeFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link historicoFragment#newInstance} factory method to
+ * Use the {@link homeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class historicoFragment extends Fragment {
+public class homeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class historicoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public historicoFragment() {
+    public homeFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class historicoFragment extends Fragment {
      * @return A new instance of fragment historicoFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static historicoFragment newInstance(String param1, String param2) {
-        historicoFragment fragment = new historicoFragment();
+    public static homeFragment newInstance(String param1, String param2) {
+        homeFragment fragment = new homeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class historicoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historico, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
